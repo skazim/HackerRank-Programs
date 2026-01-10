423fccc449bde5306994d8f3c6c1cae052b17c6c
syedkazimhussain <kazimmuhammad10@gmail.com>
2026-01-10 10:23:11 +0100

    adding CountResponseTimeRegressions

---
+++ JavaPrograms/src/main/java/org/example/CountSubarraysWithSumAndMaxAtMost.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class CountSubarraysWithSumAndMaxAtMost {
+    public static long countSubarraysWithSumAndMaxAtMost(List<Integer> nums, long k, long M) {
+
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+
+            
+        }
+        return 0;
+
+    }
+}
--- JavaPrograms/src/main/java/org/example/Main.java
+++ JavaPrograms/src/main/java/org/example/Main.java
@@ -1,10 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Fabonacii.fabonacii(10));
+        List<Integer> responseTimes = Arrays.asList(100, 200, 150,300);
+
+        List<List<Integer>> intervals = new ArrayList<>();
+
+        intervals.add(Arrays.asList(1,3));
+        intervals.add(Arrays.asList(15,18));
+        intervals.add(Arrays.asList(8,10));
+        intervals.add(Arrays.asList(2,6));
+
+//        System.out.println("Fibonacci " + Fabonacii.fabonacii(10));
+//        System.out.println("CountResponseTimeRegressions " + CountResponseTimeRegressions.countResponseTimeRegressions(responseTimes));
+
+        List<Integer> binarySearchArray = Arrays.asList(2,4,5,6,7,8,9,10);
+        List<Integer> missingNumberArray = Arrays.asList(3, 4, -1, 1);
+//        System.out.println("mergeAndSortIntervals " + mergeAndSortIntervals.mergeHighDefinitionIntervals(intervals));
+//        System.out.print("binarySearchArray : " + BinarySearch.binarySearch(binarySearchArray,10));
+        System.out.print(""+FindMissingNumber.findMissingNumber(missingNumberArray));
     }
 }
